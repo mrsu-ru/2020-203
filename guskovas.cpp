503867c1c9eccd16cf273328775f3b5b62337c24
malfiyy <rva2012@yandex.ru>
2020-02-04 15:54:03 +0300

    lab1

---
+++ guskovas.cpp
@@ -0,0 +1,86 @@
+#include "guskovas.h"
+
+/**
+ * Введение в дисциплину
+ */
+void guskovas::lab1()
+{
+  cout << "hello world!" << endl;
+}
+
+
+/**
+ * Метод Гаусса с выбором главного элемента
+ */
+void guskovas::lab2()//A, n
+{
+
+}
+
+
+
+/**
+ * Метод прогонки
+ */
+void guskovas::lab3()
+{
+
+}
+
+
+
+/**
+ * Метод простых итераций
+ */
+void guskovas::lab4()
+{
+
+}
+
+
+
+/**
+ * Метод Якоби или Зейделя
+ */
+void guskovas::lab5()
+{
+
+}
+
+
+
+/**
+ * Метод минимальных невязок
+ */
+void guskovas::lab6()
+{
+
+}
+
+
+
+/**
+ * Метод сопряженных градиентов
+ */
+void guskovas::lab7()
+{
+
+}
+
+
+void guskovas::lab8()
+{
+
+}
+
+
+void guskovas::lab9()
+{
+
+}
+
+
+std::string guskovas::get_name()
+{
+  return "Guskov A.S.";
+}
