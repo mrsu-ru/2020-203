5e586f354810a6564f3c4552ec9f6dfe15b24878
MaksimG20 <garinmaksm@gmail.com>
2020-02-04 16:08:00 +0300

    lab1

---
+++ garinma.cpp
@@ -0,0 +1,86 @@
+﻿#include "garinma.h"
+
+/**
+ * Введение в дисциплину
+ */
+void garinma::lab1()
+{
+  cout << "hello world!" << endl;
+}
+
+
+/**
+ * Метод Гаусса с выбором главного элемента
+ */
+void garinma::lab2()
+{
+
+}
+
+
+
+/**
+ * Метод прогонки
+ */
+void garinma::lab3()
+{
+
+}
+
+
+
+/**
+ * Метод простых итераций
+ */
+void garinma::lab4()
+{
+
+}
+
+
+
+/**
+ * Метод Якоби или Зейделя
+ */
+void garinma::lab5()
+{
+
+}
+
+
+
+/**
+ * Метод минимальных невязок
+ */
+void garinma::lab6()
+{
+
+}
+
+
+
+/**
+ * Метод сопряженных градиентов
+ */
+void garinma::lab7()
+{
+
+}
+
+
+void garinma::lab8()
+{
+
+}
+
+
+void garinma::lab9()
+{
+
+}
+
+
+std::string garinma::get_name()
+{
+  return "Garin M.A.";
+}
