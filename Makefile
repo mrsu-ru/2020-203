7ed2921a1d60b2804caa24e94c76a4e2142b8ee3
Борисов Александр Юрьевич <borisovayu@lab227.local>
2020-02-04 16:12:51 +0300

    lab1

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ LDFLAGS= -g
 SOURCES= \
     main.cpp \
     zhalninrv.cpp \
+	borisovayu.cpp \
 	lab.cpp
 
 OBJECTS=$(SOURCES:.cpp=.o)
---
+++ borisovayu.cpp
@@ -0,0 +1,86 @@
+﻿#include "borisovayu.h"
+
+/**
+ * Введение в дисциплину
+ */
+void borisovayu::lab1()
+{
+  cout << "hello world!" << endl;
+}
+
+
+/**
+ * Метод Гаусса с выбором главного элемента
+ */
+void borisovayu::lab2()
+{
+
+}
+
+
+
+/**
+ * Метод прогонки
+ */
+void borisovayu::lab3()
+{
+
+}
+
+
+
+/**
+ * Метод простых итераций
+ */
+void borisovayu::lab4()
+{
+
+}
+
+
+
+/**
+ * Метод Якоби или Зейделя
+ */
+void borisovayu::lab5()
+{
+
+}
+
+
+
+/**
+ * Метод минимальных невязок
+ */
+void borisovayu::lab6()
+{
+
+}
+
+
+
+/**
+ * Метод сопряженных градиентов
+ */
+void borisovayu::lab7()
+{
+
+}
+
+
+void borisovayu::lab8()
+{
+
+}
+
+
+void borisovayu::lab9()
+{
+
+}
+
+
+std::string borisovayu::get_name()
+{
+  return "Borisov A. Yu.";
+}
