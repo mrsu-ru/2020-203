953d2f9d814ef93973fb6998146f4b90aa9ef123
Ruslan Zhalnin <zhrv@mrsu.ru>
2020-02-04 13:45:09 +0300

    Init

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <cstdlib>
+#include <cstring>
+#include "lab.h"
+#include <iostream>
+#include "zhalninrv.h"
+
+
+void print_usage(char* name);
+
+
+int main(int argc, char** argv)
+{
+  if (argc < 3) {
+    print_usage(argv[0]);
+    return 0;
+  }
+
+  lab *l = NULL;
+  if (strcmp(argv[1], "zhalninrv") == 0) {
+    l = new zhalninrv();
+  }
+//  else if (strcmp(argv[1], "borisovaem") == 0) {
+//    l = new borisovaem();
+//  }
+  else  {
+    print_usage(argv[0]);
+    return 0;
+  }
+
+  l->read_file();
+  l->run(atoi(argv[2]));
+  l->write_result();
+  l->check_result();
+
+  //delete l; // TODO:
+  return 0;
+}
+
+
+void print_usage(char* name)
+{
+  std::cout << "Usage:\n\n  " << name << " <fio> <lab_number>\n";
+}
---
+++ zhalninrv.cpp
@@ -0,0 +1,86 @@
+﻿#include "zhalninrv.h"
+
+/**
+ * Введение в дисциплину
+ */
+void zhalninrv::lab1()
+{
+  cout << "hello world!" << endl;
+}
+
+
+/**
+ * Метод Гаусса с выбором главного элемента
+ */
+void zhalninrv::lab2()
+{
+
+}
+
+
+
+/**
+ * Метод прогонки
+ */
+void zhalninrv::lab3()
+{
+
+}
+
+
+
+/**
+ * Метод простых итераций
+ */
+void zhalninrv::lab4()
+{
+
+}
+
+
+
+/**
+ * Метод Якоби или Зейделя
+ */
+void zhalninrv::lab5()
+{
+
+}
+
+
+
+/**
+ * Метод минимальных невязок
+ */
+void zhalninrv::lab6()
+{
+
+}
+
+
+
+/**
+ * Метод сопряженных градиентов
+ */
+void zhalninrv::lab7()
+{
+
+}
+
+
+void zhalninrv::lab8()
+{
+
+}
+
+
+void zhalninrv::lab9()
+{
+
+}
+
+
+std::string zhalninrv::get_name()
+{
+  return "Zhalnin R.V.";
+}
