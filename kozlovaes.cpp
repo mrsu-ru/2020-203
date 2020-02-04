1b8aa330649a7416099fcb808210f7fd44bed920
Kozlova Ekaterina <kozloovakaty2000@gmail.com>
2020-02-04 15:54:14 +0300

    lab1

---
+++ kozlovaes.cpp
@@ -0,0 +1,86 @@
+﻿#include "kozlovaes.h"
+
+/**
+ * Введение в дисциплину
+ */
+void kozlovaes::lab1()
+{
+  cout << "hello world!" << endl;
+}
+
+
+/**
+ * Метод Гаусса с выбором главного элемента
+ */
+void kozlovaes::lab2()
+{
+
+}
+
+
+
+/**
+ * Метод прогонки
+ */
+void kozlovaes::lab3()
+{
+
+}
+
+
+
+/**
+ * Метод простых итераций
+ */
+void kozlovaes::lab4()
+{
+
+}
+
+
+
+/**
+ * Метод Якоби или Зейделя
+ */
+void kozlovaes::lab5()
+{
+
+}
+
+
+
+/**
+ * Метод минимальных невязок
+ */
+void kozlovaes::lab6()
+{
+
+}
+
+
+
+/**
+ * Метод сопряженных градиентов
+ */
+void kozlovaes::lab7()
+{
+
+}
+
+
+void kozlovaes::lab8()
+{
+
+}
+
+
+void kozlovaes::lab9()
+{
+
+}
+
+
+std::string kozlovaes::get_name()
+{
+  return "Kozlova E.S.";
+}
