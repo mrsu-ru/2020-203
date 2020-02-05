6c170f365ef92238ebe959973461ecfdce73455a
Сайфетдинов Салават Фаильевич <saifetdinovsf@lab227.local>
2020-02-05 11:17:09 +0300

    lab1

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ LDFLAGS= -g
 SOURCES= \
     main.cpp \
     zhalninrv.cpp \
+	sayfetdinovsf.cpp \
 	lab.cpp
 
 OBJECTS=$(SOURCES:.cpp=.o)
---
+++ sayfetdinovsf.cpp
@@ -0,0 +1,86 @@
+﻿#include "sayfetdinovsf.h"
+
+/**
+ * Введение в дисциплину
+ */
+void sayfetdinovsf::lab1()
+{
+  cout << "hello world!" << endl;
+}
+
+
+/**
+ * Метод Гаусса с выбором главного элемента
+ */
+void sayfetdinovsf::lab2()
+{
+
+}
+
+
+
+/**
+ * Метод прогонки
+ */
+void sayfetdinovsf::lab3()
+{
+
+}
+
+
+
+/**
+ * Метод простых итераций
+ */
+void sayfetdinovsf::lab4()
+{
+
+}
+
+
+
+/**
+ * Метод Якоби или Зейделя
+ */
+void sayfetdinovsf::lab5()
+{
+
+}
+
+
+
+/**
+ * Метод минимальных невязок
+ */
+void sayfetdinovsf::lab6()
+{
+
+}
+
+
+
+/**
+ * Метод сопряженных градиентов
+ */
+void sayfetdinovsf::lab7()
+{
+
+}
+
+
+void sayfetdinovsf::lab8()
+{
+
+}
+
+
+void sayfetdinovsf::lab9()
+{
+
+}
+
+
+std::string sayfetdinovsf::get_name()
+{
+  return "sayfetdinovsf";
+}
