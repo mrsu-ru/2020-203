7afffa46d4e7801e4480f5c3892618029b127c76
Irina <iskazakakova1202@gmail.com>
2020-02-11 23:44:52 +0300

    variant of lab2

--- kazakovais.cpp
+++ kazakovais.cpp
@@ -14,40 +14,94 @@ void kazakovais::lab1()
  */
 void kazakovais::lab2()
 {
-int i,j,k,max;
+    //double eps = 0.0001;
+    
+    double *vspom = new double[N];
+    double vspom_1;
+    
+    
+	int max,i,j,k;
+	double sum;
+	sum = 0;
 
-  for (i=0; i<N; b[i]=-b[i], i++)
+	for (i=0; i<N; i++)
+	{
+		b[i]=-b[i];
+	}
+	
+	//прямой ход
+	for (i = 0; i < N; i++)
+	{
+		max = i;
+		for (j = i + 1; j < N; j++)
+		{
+			if (fabs(A[j][i]) > fabs(A[max][i]))
+			{
+				max = j;
+			}
+		
+		
+		
+			
+		}
+		
+		
+		
+			
+			if (max != i)
+			{
+				for (k = 0; k < N; k++)
+				{
+	                //swap(A[i][k],A[max][k]);
+	                vspom[k]=A[i][k];
+	                A[i][k]=A[max][k];
+	                A[max][k]=vspom[k];
+	                
+                }
+				//swap(b[i], b[max]);
+				vspom_1=b[i];
+				b[i]=b[max];
+				b[max]=vspom_1;
+			}
+/*
+			if ((A[i][i] < 0 + eps) && (A[i][i] > 0 - eps))
+			{
+				cout << "Решений нет." << endl;
+				break;
+			}
+*/
+//		}
+		
+		for (j = N-1; j > i; j--)
+		{
+			A[i][j] /= A[i][i];
+		}
+		
+		b[i]/=A[i][i];
+		
+		A[i][i] = 1;
+		for (j = i + 1; j < N; j++)
+		{
+			for (k = N-1; k > i; k--)
+			{
+				A[j][k] = A[j][k] - A[j][i] * A[i][k];
+			}
+			b[j]=b[j]-A[j][i]*b[i];
+			A[j][i] = 0;
+		}
+	}
 
-  for (i=0; i<N; i++) {
-      max=i;
-      double* s; double s1;
-      for (j=i+1; j<N; j++) if (A[j][i]*A[j][i]>A[max][i]*A[max][i]) max=j;
-      if (max!=i) {
-         s=A[i]; s1=b[i];
-         A[i]=A[max]; b[i]=b[max];
-         A[max]=s; b[max]=s1;
-      }
-
-      for (j=N-1; j>i; A[i][j]/=A[i][i], j--);
-      b[i]/=A[i][i];
-      A[i][i]=1;
-
-      for (j=N-1; j>i; j--) {
-          for (k=i+1; k<N; A[j][k]-=A[i][k]*A[j][i], k++);
-          b[j]-=b[i]*A[j][i];
-          A[j][i]=0;
-      }
-
-
-  }
-
-  double sum;
-  x[N-1]=b[N-1];
-  for (i=N-2;i>=0;i--){
-    sum=0;
-    for (j=i+1;j<N;j++) sum+=x[j]*A[i][j];
-    x[i]=b[i]-sum;
-  }
+	//обратный ход
+	x[N-1]=b[N-1];
+	for (i = N - 2; i > -1; i--)
+	{
+		for (j = i + 1; j < N; j++)
+		{
+			sum += x[j] * A[i][j];
+		}
+		x[i] = b[i] - sum;
+		sum = 0;
+	}
 	
 }
 
