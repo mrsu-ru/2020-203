feb9a11d676955f6d3631f7e80a6068385aa9e46
malfiyy <rva2012@yandex.ru>
2020-02-11 16:51:59 +0300

    lab2

--- guskovas.cpp
+++ guskovas.cpp
@@ -12,9 +12,36 @@ void guskovas::lab1()
 /**
  * Метод Гаусса с выбором главного элемента
  */
-void guskovas::lab2()//A, n
+void guskovas::lab2()//int n, int m, double e, double** arr, double* x) {
 {
+	//ТУДА
+	int n = N;
 
+	for(int k = 0; k < n-1; k++){
+		for(int i = k+1; i < n; i++){
+			double c = A[i][k] / A[k][k];
+			//A[i][k] = 0;
+			for(int j = k+1; j < n; j++){
+				A[i][j] -= A[k][j] * c; 
+			}
+			b[i] -= b[k] * c;
+		}
+	}
+
+	for (int i = 0; i < n; i++){
+			x[i] = b[i];
+		}
+
+	//Обратно
+
+	for(int i = n-1; i >= 0; i--){
+		double S = 0;
+		for(int j = i+1; j < n; j++){
+			x[i] -= A[i][j] * x[j];
+		}
+		x[i] /= A[i][i];
+	}
+	
 }
 
 
