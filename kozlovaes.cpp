57d70b37ee110cc73c465407bc814ca5033ebecb
Kozlova Ekaterina <kozloovakaty2000@gmail.com>
2020-02-12 10:15:18 +0300

    comment

--- kozlovaes.cpp
+++ kozlovaes.cpp
@@ -14,7 +14,62 @@ void kozlovaes::lab1()
  */
 void kozlovaes::lab2()
 {
+cout << "\nПрямой ход: " << endl;
+	for (int i = 0; i < N; i++) {
+		int max = i;
+		for (int j = i + 1; j < N; j++) {
+			if (abs(A[j][i]) > abs(A[max][i]))
+				max = j;
+		}
 
+		if (abs(A[i][i]) <= eps) {
+			cout<<"Нет решений!";
+		}
+
+		double *s;
+		double tmp;
+		if (max != i) {
+			s = A[i]; A[i] = A[max]; A[max] = s;
+			tmp = b[i]; b[i] = b[max]; b[max] = tmp;
+		}
+		cout << endl;
+		print(N, A);
+		print1(N, b);
+
+		for (int j = N; j > i; A[i][j--] /= A[i][i]);
+		b[i] /= A[i][i];
+		A[i][i] = 1;
+		cout << endl;
+		print(N, A);
+		print1(N, b);
+
+		for (int j = i + 1; j < N; j++) {
+
+			for (int k = N; k > i; A[j][k] -= A[i][k--] * A[j][i]);
+			b[j] -= b[i] * A[j][i];
+			A[j][i] = 0;
+		}
+		print1(N, b);
+
+		cout << endl;
+		print(N, A);
+	}
+	cout << "\n\nОбратный ход: " << endl;
+	for (int i = N - 1; i > 0; i--) {
+		double s = 0;
+		for (int j = i ; j < N; j++) {
+			s += b[j] * A[i - 1][j];
+			A[i - 1][j] = 0;
+		}
+		b[i - 1] -=s;
+		cout << endl;
+		print1(N, b);
+		print(N, A);
+	}
+
+	for (int i = 0; i < N; i++) {
+		x[i] = b[i];
+	}
 }
 
 
