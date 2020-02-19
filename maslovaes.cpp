2392281d1551e11a94e999b968618a9abd9c1fb2
Маслова Елена Сергеевна <maslovaes@lab227.local>
2020-02-19 11:37:56 +0300

    lab2

--- maslovaes.cpp
+++ maslovaes.cpp
@@ -14,7 +14,40 @@ void maslovaes::lab1()
  */
 void maslovaes::lab2()
 {
+for (int k = 0; k < N; k++) {
+	int idmax = 0;
+	for (int i=0; i<N; i++) {
+		if(abs(A[i][k]) > abs(A[idmax][k])) idmax = i; 
+	}
 
+	for (int i = 0; i < N; i++)
+	{
+		swap(A[idmax][i], A[k][i]);
+	}
+	swap(b[idmax], b[k]);
+
+	//down
+	for (int i = k + 1; i < N; i++)
+	{
+		double tmp = A[i][k]/A[k][k];
+		for (int j = k; j < N; j++) {
+			A[i][j] -= A[k][j]*tmp;
+		}
+		b[i] -= b[k]*tmp;
+	}
+}
+
+for(int i = 0; i<N; i++){
+    x[i]=b[i];
+}
+
+//up
+for (int i = N-1; i >= 0; i--){
+	for (int j = i+1; j < N; j++) {
+		x[i] -= A[i][j]*x[j];
+	}
+	x[i] /= A[i][i];
+}
 }
 
 
