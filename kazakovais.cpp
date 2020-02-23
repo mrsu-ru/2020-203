7d1e1699707b7bed7cdf6c0fd5e04dbfa7997520
Irina <iskazakakova1202@gmail.com>
2020-02-23 23:53:23 +0300

    lab3 test

--- kazakovais.cpp
+++ kazakovais.cpp
@@ -80,6 +80,59 @@ void kazakovais::lab2()
  */
 void kazakovais::lab3()
 {
+	/*
+	double *alpha;
+	double *beta;
+	alpha = new double[N];  //массивы для прогоночных коэффициентов
+	beta = new double[N];
+
+	alpha[0] = A[0][1]/A[0][0];		//начальные коэффициенты
+	//beta[0] = -b[0]/A[0][0];
+	beta[0] = b[0]/A[0][0];
+
+	//прямой ход - нахождение коэффициентов
+	for (int i=1;i<N;i++)
+	{
+		alpha[i] = A[i][i+1]/(A[i][i]-A[i][i-1]*alpha[i-1]);
+		beta[i] = (A[i][i-1]*beta[i-1]-b[i])/(A[i][i]-A[i][i-1]*alpha[i-1]);
+
+	}
+
+
+	//обратный ход - нахождение решений
+	x[N-1] = b[N-1];
+	//x[N-2] = alpha[N-2]*x[N-1]+beta[N-2];
+
+	for (int j= N-2;j>=0;j--)
+	{
+	x[j] = alpha[j]*x[j+1]+beta[j];
+	}
+	*/
+
+	double *alpha;
+	double *beta;
+	alpha = new double[N];  //массивы для прогоночных коэффициентов
+	beta = new double[N];
+
+	//прямой ход
+	alpha[0] = -A[0][1] / A[0][0];
+	beta[0] = b[0] / A[0][0];
+
+	for (int i = 1; i < N - 1; i++)
+	{
+		alpha[i] = -A[i][i + 1] / (A[i][i] + A[i][i - 1] * alpha[i - 1]);
+		beta[i] = (b[i] - A[i][i - 1] * beta[i - 1]) / (A[i][i] + A[i][i - 1] * alpha[i - 1]);
+	}
+
+	beta[N - 1] = (b[N - 1] - A[N - 1][N - 2] * beta[N - 2]) / (A[N - 1][N - 1] + A[N - 1][N - 2] * alpha[N - 2]);
+
+	//обратный ход
+	x[N - 1] = beta[N - 1];
+
+	for (int i = N - 2; i >= 0; i--)
+	{
+		x[i] = alpha[i] * x[i + 1] + beta[i];
+	}
 
 }
 
