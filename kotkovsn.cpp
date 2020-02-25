7e7bf6b7859c77447d283e31eb92e86e96632900
SNKotV <48939733+SNKotV@users.noreply.github.com>
2020-02-25 20:50:34 +0300

    lab3

--- kotkovsn.cpp
+++ kotkovsn.cpp
@@ -63,13 +63,28 @@ void kotkovsn::lab2()
 }
 
 
-
+ 
 /**
  * Метод прогонки
  */
 void kotkovsn::lab3()
 {
+  double *alpha = new double[N];
+  double *beta = new double[N];
 
+  alpha[0] = A[0][1] / A[0][0];
+  beta[0] = b[0] / A[0][0];
+
+  for (int i = 1; i < N; i++)
+  {
+    alpha[i] = A[i][i + 1] / (A[i][i] - A[i][i - 1] * alpha[i - 1]);
+    beta[i] = (b[i] - A[i][i - 1] * beta[i - 1]) / (A[i][i] - A[i][i - 1] * alpha[i - 1]);
+  }
+
+  x[N - 1] = beta[N - 1];
+
+  for (int i = N - 2; i >= 0; i--)
+    x[i] = beta[i] - alpha[i] * x[i + 1];
 }
 
 
