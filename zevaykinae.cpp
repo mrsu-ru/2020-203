fc57ef69385b508824f4905dd4565fd0736ebac2
Andrey Zevaykin <mxskorpion@gmail.com>
2020-02-25 17:06:53 +0300

    no message

--- zevaykinae.cpp
+++ zevaykinae.cpp
@@ -1,9 +1,9 @@
-﻿#include "zhalninrv.h"
+﻿#include "zevaykinae.h"
 
 /**
  * Введение в дисциплину
  */
-void zhalninrv::lab1()
+void zevaykinae::lab1()
 {
   cout << "hello world!" << endl;
 }
@@ -12,7 +12,7 @@ void zhalninrv::lab1()
 /**
  * Метод Гаусса с выбором главного элемента
  */
-void zhalninrv::lab2()
+void zevaykinae::lab2()
 {
 
 }
@@ -22,7 +22,7 @@ void zhalninrv::lab2()
 /**
  * Метод прогонки
  */
-void zhalninrv::lab3()
+void zevaykinae::lab3()
 {
 
 }
@@ -32,7 +32,7 @@ void zhalninrv::lab3()
 /**
  * Метод простых итераций
  */
-void zhalninrv::lab4()
+void zevaykinae::lab4()
 {
 
 }
@@ -42,7 +42,7 @@ void zhalninrv::lab4()
 /**
  * Метод Якоби или Зейделя
  */
-void zhalninrv::lab5()
+void zevaykinae::lab5()
 {
 
 }
@@ -52,7 +52,7 @@ void zhalninrv::lab5()
 /**
  * Метод минимальных невязок
  */
-void zhalninrv::lab6()
+void zevaykinae::lab6()
 {
 
 }
@@ -62,25 +62,25 @@ void zhalninrv::lab6()
 /**
  * Метод сопряженных градиентов
  */
-void zhalninrv::lab7()
+void zevaykinae::lab7()
 {
 
 }
 
 
-void zhalninrv::lab8()
+void zevaykinae::lab8()
 {
 
 }
 
 
-void zhalninrv::lab9()
+void zevaykinae::lab9()
 {
 
 }
 
 
-std::string zhalninrv::get_name()
+std::string zevaykinae::get_name()
 {
-  return "Zhalnin R.V.";
+  return "Zevaykin A.E.";
 }
