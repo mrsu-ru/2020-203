41e038d0834a3be8e3154788625d15778e127650
Andrey Zevaykin <mxskorpion@gmail.com>
2020-02-25 17:15:16 +0300

    no message

--- zevaykinae.cpp
+++ zevaykinae.cpp
@@ -14,7 +14,23 @@ void zevaykinae::lab1()
  */
 void zevaykinae::lab2()
 {
-
+	for (int i = 0; i < N; i++) 
+		x[i] = b[i]; 
+	long double m; 
+	for (int k = 0; k < N - 1; k++) { 
+		for (int i = k + 1; i < N; i++) { 
+			m = A[i][k] / A[k][k]; 	
+			for (int j = k; j < N; j++) { 
+				A[i][j] = A[i][j] - m * A[k][j]; 
+			} 
+			x[i] = x[i] - m * x[k]; 
+		} 
+	} 
+	for (int i = N - 1; i >= 0; i--) { 
+		for (int j = i + 1; j < N; j++) 
+		x[i] = x[i] - A[i][j] * x[j]; 
+		x[i] = x[i] / A[i][i]; 
+	} 
 }
 
 
