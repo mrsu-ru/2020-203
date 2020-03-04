77cf6fc201347513d8d29ff7f0969b6db52d1f91
Горбунов Андрей Александрович <gorbunovaa@lab227.local>
2020-03-04 10:05:08 +0200

    Lab1

---
+++ gorbunovaa.cpp
@@ -0,0 +1,86 @@
+#include "gorbunovaa.h"
+
+/**
+ * ¬ведение в дисциплину
+ */
+void gorbunovaa::lab1()
+{
+  cout << "hello world!" << endl;
+}
+
+
+/**
+ * ћетод √аусса с выбором главного элемента
+ */
+void gorbunovaa::lab2()
+{
+
+}
+
+
+
+/**
+ * ћетод прогонки
+ */
+void gorbunovaa::lab3()
+{
+
+}
+
+
+
+/**
+ * ћетод простых итераций
+ */
+void gorbunovaa::lab4()
+{
+
+}
+
+
+
+/**
+ * ћетод якоби или «ейдел€
+ */
+void gorbunovaa::lab5()
+{
+
+}
+
+
+
+/**
+ * ћетод минимальных нев€зок
+ */
+void gorbunovaa::lab6()
+{
+
+}
+
+
+
+/**
+ * ћетод сопр€женных градиентов
+ */
+void gorbunovaa::lab7()
+{
+
+}
+
+
+void gorbunovaa::lab8()
+{
+
+}
+
+
+void gorbunovaa::lab9()
+{
+
+}
+
+
+std::string gorbunovaa::get_name()
+{
+  return "Gorbunov A. A.";
+}
