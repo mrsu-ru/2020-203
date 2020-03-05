a895403f473d924035431f75720ea89088217be2
Казакова Ирина Сергеевна <kazakovais@lab227.local>
2020-03-05 16:03:52 +0300

    lab1

---
+++ kazakovais.cpp
@@ -0,0 +1,86 @@
+﻿#include "kazakovais.h"
+
+/**
+ * Введение в дисциплину
+ */
+void kazakovais::lab1()
+{
+  cout << "hello world!" << endl;
+}
+
+
+/**
+ * Метод Гаусса с выбором главного элемента
+ */
+void kazakovais::lab2()
+{
+
+}
+
+
+
+/**
+ * Метод прогонки
+ */
+void kazakovais::lab3()
+{
+
+}
+
+
+
+/**
+ * Метод простых итераций
+ */
+void kazakovais::lab4()
+{
+
+}
+
+
+
+/**
+ * Метод Якоби или Зейделя
+ */
+void kazakovais::lab5()
+{
+
+}
+
+
+
+/**
+ * Метод минимальных невязок
+ */
+void kazakovais::lab6()
+{
+
+}
+
+
+
+/**
+ * Метод сопряженных градиентов
+ */
+void kazakovais::lab7()
+{
+
+}
+
+
+void kazakovais::lab8()
+{
+
+}
+
+
+void kazakovais::lab9()
+{
+
+}
+
+
+std::string kazakovais::get_name()
+{
+  return "Kazakova I.S.";
+}
