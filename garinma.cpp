75c0a855fce561d6b59526c312ebe054c4cce0a3
MaksimG20 <garinmaksm@gmail.com>
2020-03-26 12:07:06 +0300

    lab4

--- garinma.cpp
+++ garinma.cpp
@@ -91,8 +91,38 @@ x[i] = alfa[i] * x[i + 1] + beta[i];
  */
 void garinma::lab4()
 {
+  double *new_x = new double[N],
+  tau = 0.001,
+  eps = 1.e-10;
 
+  for (int i = 0; i < N; i++)
+        x[i] = 0;
+
+    do
+    {
+        for (int i = 0; i < N; i++)
+        {
+            double temp = 0;
+            for (int j = 0; j < N; j++)
+                temp += A[i][j] * x[j];
+
+            new_x[i] = x[i] + tau * (b[i] - temp);
+        }
+
+        double maxdif = 0;
+        for (int i = 0; i < N; i++)
+        {
+            if (fabs(x[i] - new_x[i]) > maxdif) maxdif = fabs(x[i] - new_x[i]);
+            x[i] = new_x[i];
+        }
+
+        if (maxdif < eps) break;
+    }while(true);
+
+    delete[] new_x;
 }
+
+
 
 
 
