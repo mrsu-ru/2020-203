e4a6fb9ffa1478618e7083de249c6a9b6ed89f4f
simatovvv <grikss2000@gmail.com>
2020-04-08 10:23:10 +0300

    lab4

--- simatovvv.cpp
+++ simatovvv.cpp
@@ -71,7 +71,48 @@ x[i] = alpha[i] * x[i + 1] + beta[i];
  */
 void simatovvv::lab4()
 {
+double** L = new double*[N];
+	for (int i=0; i<N; i++)
+	L[i] = new double[N];
+	double* y = new double[N];
 
+   for (int i=0; i<N; i++)
+   {
+		for (int j=0; j<N; j++)
+			{
+				L[i][j]=0;
+			}
+   }
+
+  for(int i=0;i<N;i++)
+	{
+		for(int k=0;k<=i-1;k++)
+
+			L[i][i]+=L[i][k]*L[i][k];
+			L[i][i]=sqrt(A[i][i]-L[i][i]);
+			for(int j=i+1;j<N;j++)
+				{
+					for(int k=0;k<=i-1;k++)
+						L[j][i]+=L[i][k]*L[j][k];
+						L[j][i]=(A[i][j]-L[j][i])/L[i][i];
+				}
+  }
+	double summa=0;
+    for(int i=0;i<N;i++)
+		{
+			for(int j=0;j<i;j++){
+			summa+=L[i][j]*y[j];}
+			y[i]=(b[i]-summa)/L[i][i];
+			summa=0;
+		}
+	for(int i=N-1;i>=0;i--)
+		{
+			for(int j=i+1;j<N;j++){
+				summa+=L[j][i]*x[j];}
+				x[i]=(y[i]-summa)/L[i][i];
+				summa=0;
+		}
+delete[] y;
 }
 
 
