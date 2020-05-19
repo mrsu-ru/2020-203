c3c7f1d7df09c28e02c9167a03e6d9c2e3c6ba39
Dekinus <kirill060309@gmail.com>
2020-05-19 14:58:29 +0300

    Test2

--- kvashninka.cpp
+++ kvashninka.cpp
@@ -5,7 +5,7 @@
  */
 void kvashninka::lab1()
 {
-  cout << "hello world!" << endl;
+    cout << "hello world!" << endl;
 }
 
 
@@ -14,7 +14,59 @@ void kvashninka::lab1()
  */
 void kvashninka::lab2()
 {
-	//test
+    int *sw1 = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        sw1[i] = i;
+    }
+    for (int i = 0; i < N; i++)
+    {
+        int Maxi = i;
+        for (int j = i + 1; j < N; j++)
+        {
+            if (abs(A[j][i]) > abs(A[Maxi][i]))
+            {
+                Maxi = j;
+            }
+        }
+        if (Maxi != i)
+        {
+            swap(A[Maxi], A[i]);
+            swap(b[Maxi], b[i]);
+            swap(sw1[Maxi], sw1[i]);
+        }
+        b[i] /= A[i][i];
+        for (int j = N - 1; j >= i; j--)
+        {
+            A[i][j] /= A[i][i];
+        }
+
+        for (int j = 0; j < N; j++)
+        {
+            if (j != i)
+            {
+                b[j] -= A[j][i] * b[i];
+                for (int k = N - 1; k >= i; k--)
+                {
+                    A[j][k] -= A[j][i] * A[i][k];
+                }
+
+            }
+
+        }
+    }
+    for (int i = 0; i < N; i++)
+    {
+        x[i] = b[i];
+    }
+    for (int i = 0; i < N; i++)
+    {
+        if (sw1[i] != i)
+        {
+            swap(A[sw1[i]], A[i]);
+            swap(b[sw1[i]], b[i]);
+        }
+    }
 }
 
 
@@ -82,5 +134,5 @@ void kvashninka::lab9()
 
 std::string kvashninka::get_name()
 {
-  return "Kvashnin K.A.";
+    return "Kvashnin K.A.";
 }
