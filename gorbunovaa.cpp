fb172a5db70de3510c41810ecac0174037634994
Andrey <mr.spy2000@mail.ru>
2020-05-20 13:03:12 +0300

    добавил 2-ю лаб

--- gorbunovaa.cpp
+++ gorbunovaa.cpp
@@ -14,68 +14,61 @@ void gorbunovaa::lab1()
  */
 void gorbunovaa::lab2()
 {
-	setlocale(LC_ALL, "Russian");
-	double* x;
-	int i, j, m, k;
-	double aa, bb;
-	// Для матрицы нужена переменная 'a'
-	x = (double *)malloc(n * sizeof(double));
+	const double e = 1e-10;
 
-	for (k = 0; k < n; k++) //Поиск максимального элемента в первом столбце
-	{
-		aa = abs(a[k][k]);
-		i = k;
-		for (m = k + 1; m < n; m++)
-			if (abs(a[m][k]) > aa)
-			{
-				i = m;
-				aa = abs(a[m][k]);
-			}
+	for (int i = 0; i < n; i++) {
+		if (fabs(A[i][i]) < e) { // Проверка диагонали на элемент меньше е (т.е. нулевой)
+			for (int j = i + 1; j < n; j++) {
+				if (fabs(A[j][i]) > e) { // Если найден такой эл., то заменям на строку с не нулевым эл.
+					double *tmp = A[i]; 
+					for (int k = 0; k < n; k++) { 
+						A[i][k] = A[j][k];
+						A[j][k] = tmp[k];
+					}
 
-		if (aa == 0)   //проверка на нулевой элемент
-		{
-			cout << "Система не имеет решений" << endl;
-		}
-
-		if (i != k)  //  перестановка i-ой строки, содержащей главный элемент k-ой строки
-		{
-			for (j = k; j < n + 1; j++)
-			{
-				bb = a[k][j];
-				a[k][j] = a[i][j];
-				a[i][j] = bb;
+					double temp = b[i];
+					b[i] = b[j];
+					b[j] = temp;
+					break;
+				}
 			}
 		}
-		aa = a[k][k];//преобразование k-ой строки (Вычисление масштабирующих множителей)
-		a[k][k] = 1;
-		for (j = k + 1; j < n; j++)
-			a[k][j] = a[k][j] / aa;
-		for (i = k + 1; i < n; i++)//преобразование строк с помощью k-ой строки
-		{
-			bb = a[i][k];
-			a[i][k] = 0;
-			if (bb != 0)
-				for (j = k + 1; j < n + 1; j++)
-					a[i][j] = a[i][j] - bb * a[k][j];
+
+		if (fabs(A[i][i] - 1) > e) {		// Сделанно для оптимизации, 
+			for (int j = i + 1; j < n; j++) // что бы элемент равный 1, лишний раз не делил все остальные эл.
+				A[i][j] /= A[i][i];
+			b[i] /= A[i][i];
+			A[i][i] = 1;
+		}
+
+		for (int j = 0; j < i; j++) {
+			for (int k = i + 1; k < n; k++)
+				A[j][k] -= A[i][k];
+			b[j] -= b[i] * A[j][i];
+			A[j][i] = 0;
+		}
+
+		for (int j = i + 1; j < n; j++) {
+			for (int k = i + 1; k < n; k++)
+				A[j][k] -= A[i][k] * A[j][i];
+			b[j] -= b[i] * A[j][i];
+			A[j][i] = 0;
+		}
+
+		for (int j = i +1; j < n; j++) {
+			for (int k = i + 1; k < n; k++)
+				A[j][k] -= A[i][k] * A[j][i];
+			b[j] -= b[i] * A[j][i];
+			A[j][i] = 0;
 		}
 	}
 
-	for (i = n; i > 0; i--)   //Нахождение решений СЛАУ
-	{
-		x[i] = 0;
-		aa = a[i][n + 1];
-		for (j = n; j > i; j--)
-			aa = aa - a[i][j] * x[j];
-		x[i] = aa;
-	}
+	for (int i = 0; i < n; i++)
+		x[i] = b[i];
 
-	cout << "Решение системы:" << endl;  //вывод решений
-	for (i = 1; i < n + 1; i++)
-	{
-		cout << "x[" << i << "]=" << x[i];
-		cout << endl;
+	for (int i = 0; i < n; i++) {
+		cout << "x[" << i << "] = " << x[i] << endl;
 	}
-	system("PAUSE");
 }
 
 
