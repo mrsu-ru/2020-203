ef7cff160da24df4ff8ef95f7fabcc041df56424
Anastasia <onostosiadiepie@gmail.com>
2020-06-03 02:49:00 +0300

    lab 9 + lab 8

--- golovatyukam.cpp
+++ golovatyukam.cpp
@@ -399,16 +399,113 @@ do{
 }
 
 
-void golovatyukam::lab8()
-{
+void golovatyukam::lab8(){
+int n = N;
+double** C = new double*[n];
+double eps = 1e-10,err = 0;
+int cur_i, cur_j;
+
+for (int i = 0; i < n; i++){
+	C[i] = new double[n];
+}
+
+//вычисляем ошибку
+for (int i = 0; i < n; i++){
+  for (int j = i+1; j < n; j++){
+    if(i!=j){ 
+      err+=A[i][j];
+    }
+    C[i][j] = 0;
+  }
+}
+
+while(err>eps){
+  //cout<<1<<endl;
+  double max = abs(A[0][1]);
+  //находим max в верхнем треугольнике
+  for (int i = 0; i < n; i++){
+    for (int j = i+1; j < n; j++){
+      if(abs(A[i][j]) > max){
+        max = abs(A[i][j]);
+        cur_i = i;
+        cur_j = j;
+      }
+    }
+  }
+  double phi;
+  if(A[cur_i][cur_i]!= A[cur_j][cur_j]){
+    phi = 0.5*atan(2*A[cur_i][cur_j]/(A[cur_j][cur_j] - A[cur_i][cur_i]));
+  }else{phi = M_PI/4;}
+  double s = sin(phi), c = cos(phi);
+
+  //используем ф-лы (10)
+  C[cur_i][cur_j] = c*c*A[cur_i][cur_i] - 2*s*c*A[cur_i][cur_j] + s*s*A[cur_j][cur_j];
+  C[cur_j][cur_j]	= s*s*A[cur_i][cur_i] + 2*s*c*A[cur_i][cur_j] + c*c*A[cur_j][cur_j];
+	C[cur_i][cur_j] = (c*c - s*s)*A[cur_i][cur_j] + s*c*(A[cur_j][cur_j] - A[cur_i][cur_i]);
+	C[cur_j][cur_i] = C[cur_i][cur_j];
+
+  for (int k=0; k<n; k++){
+	  if (k != cur_i  &&  k != cur_j){
+	  	C[cur_i][k] = c*A[cur_i][k] - s*c*A[cur_j][k];
+	  	C[k][cur_i] = C[cur_i][k];
+	  	C[cur_j][k] = s*A[cur_i][k] + c*A[cur_j][k];//s*c??
+	  	C[k][cur_j] = C[cur_j][k];
+	  }
+
+    for(int l = 0; l<n; l++)
+      if(l!= cur_j && k!=cur_i)
+         C[k][l] = A[k][l];
+  }
+
+  err = 0;
+  for (int i = 0; i < n; i++)
+    for (int j = i+1; j < n; j++)
+      if(i!=j) err+=C[i][j];
+
+
+  for (int i=0; i<n; i++)
+      for (int j=0; j<n; j++)
+	      A[i][j] = C[i][j];
+}
+    cout<<"СЗ Mat(A):"<<endl;
+	for (int i=0; i<n; i++)
+		cout<<A[i][i]<<endl;
+    cout<<"))";
 
 }
 
 
-void golovatyukam::lab9()
-{
+void golovatyukam::lab9(){
+  int n = N;
+  double *y = new double[n];
+  double *y_next = new double[n];
 
+  double eps =1e-2, lyambda = 1, lyambda_next = 0;
+
+  for(int i = 0; i<n; i++)
+    y[i] = b[i];//y0 
+
+  do{
+  for(int i=0; i<n; i++){
+      for(int j=0; j<n; j++){
+        y_next[i] += A[i][j]*y[j];
+      }
+  }
+  lyambda = lyambda_next;
+
+  for(int i=0; i<n; i++){
+    if(y[i]!= 0 && y_next[i] != 0){
+      lyambda_next = y_next[i]/y[i];
+      break;
+    }
+  }
+  for (int i=0; i<n; i++)
+   	y[i]=y_next[i];
+  }while(fabs(lyambda_next - lyambda)>eps);
+
+cout<<"Answer: "<<lyambda_next << endl;
 }
+
 
 
 std::string golovatyukam::get_name()
